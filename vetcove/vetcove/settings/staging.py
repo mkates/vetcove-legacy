74df4e9731b1ef516012b569411edcb5b079ae12
Mitchell Kates <mhkates@gmail.com>
2014-09-11 08:32:13 -0400

    broken db

--- vetcove/vetcove/settings/staging.py
+++ vetcove/vetcove/settings/staging.py
@@ -55,6 +55,7 @@ SERVER_EMAIL = EMAIL_HOST_USER
 
 ########## DATABASE CONFIGURATION
 import dj_database_url
+print(dj_database_url.config())
 DATABASES['default'] =  dj_database_url.config()
 ########## END DATABASE CONFIGURATION
 
