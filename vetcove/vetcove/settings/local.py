84b2e44a5911e76558b068a64ff44c9c76753f49
Mitchell Kates <mhkates@gmail.com>
2014-09-14 17:38:12 -0400

    wtf

--- vetcove/vetcove/settings/local.py
+++ vetcove/vetcove/settings/local.py
@@ -106,7 +106,7 @@ CACHES = {
 #DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
-#INTERNAL_IPS = ('127.0.0.1',)
+INTERNAL_IPS = ('127.0.0.1',)
 ########## END TOOLBAR CONFIGURATION
 
 ############# SECRET KEY
